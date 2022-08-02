3fb36738af8d5f1b4f3f21325dd67680da50c9b5
Manuel Del Real <manueldelreal@gmail.com>
2022-08-02 18:31:23 +0000

    Add data source and change graph type

--- app.py
+++ app.py
@@ -1,71 +1,51 @@
 import dash
 import dash_auth
-from dash import html, dcc
-from dash.dependencies import Input, Output, State
-import plotly.graph_objects as go
-
+from dash import dcc
+from dash import html
+from dash import Input, Output
+import plotly.express as px
 
 # Keep this out of source code repository - save in a file or a database
 VALID_USERNAME_PASSWORD_PAIRS = {
-    'Mickey': 'Mouse', 'Donald': 'Duck'
+    'Life': 'Expectancy'
 }
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://cdn.jsdelivr.net/npm/purecss@2.1.0/build/pure-min.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title='auth example'
+app.title='Authorization Application'
 auth = dash_auth.BasicAuth(
     app,
     VALID_USERNAME_PASSWORD_PAIRS
 )
 
 app.layout = html.Div([
-    html.H1('Welcome to the app'),
+    html.H1('Welcome!'),
     html.H3('You are successfully authorized'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in [1, 2, 3, 4, 5]],
-        value=1
+    html.H4('Life expentancy progression of countries per continents'),
+    dcc.Checklist(
+        id='checklist',
+        options=["Asia", "Europe", "Africa","Americas","Oceania"],
+        value="Americas"
     ),
-
-    html.Div(id='graph-title'),
     dcc.Graph(id='graph'),
-    html.A('Code on Github', href='https://github.com/austinlasseter/dash-auth-example'),
+    html.A('Code on Github', href='https://github.com/manueldelreal/208-authentication-example'),
     html.Br(),
-    html.A("Data Source", href='https://dash.plotly.com/authentication'),
+    html.A("Data Source", href='https://www.gapminder.org/tag/life-expectancy/'),
 ], className='container')
 
+
 @app.callback(
-    Output('graph-title', 'children'),
-    Output('graph', 'figure'),
-    Input('dropdown', 'value'),
-    )
-def update_graph(dropdown_value):
+    Output("graph", "figure"), 
+    Input("checklist", "value"))
 
-    x_values = [-3,-2,-1,0,1,2,3]
-    y_values = [x**dropdown_value for x in x_values]
-    colors=['black','red','green','blue','orange','purple']
-    graph_title='Graph of {}'.format(str(dropdown_value))
-
-
-    trace0 = go.Scatter(
-        x = x_values,
-        y = y_values,
-        mode = 'lines',
-        marker = {'color': colors[dropdown_value]},
-    )
-
-    # assign traces to data
-    data = [trace0]
-    layout = go.Layout(
-        title = graph_title
-    )
-
-    # Generate the figure dictionary
-    fig = go.Figure(data=data,layout=layout)
-
-    return graph_title, fig
+def update_line_chart(value):
+    df = px.data.gapminder() # replace with your own data source
+    mask = df.continent.isin(value)
+    fig = px.line(df[mask], 
+        x="year", y="lifeExp", color='country')
+    return fig
 
 
 ############ Deploy
